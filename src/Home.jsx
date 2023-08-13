6765358b62bc3ed85fce4dd2dc9ddc69b09344f9
ariel593 <arielferaguirre.2001@gmail.com>
2023-08-12 22:28:32 -0500

    correciones

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,11 +4,8 @@ import "./index.css";
 export default function Home() {
   return (
     <div className="centered">
-      <h1>UCSG</h1>
-      <img src="src/assets/logo.png" />
       <h1>Universidad Católica de Santiago de Guayaquil</h1>
-
-      <img src="src/assets/logo.png" />
+      <img src="logo.png" />
       <h3>Ingresa para votar</h3>
       <LoginButton />
     </div>
